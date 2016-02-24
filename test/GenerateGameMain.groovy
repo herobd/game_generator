a75c5521119bcf9eed9d104242a1228fbaa5f1e5
Lawrence Thatcher <lwthatcher@msn.com>
2016-02-24 16:27:02 -0700

    added Main class/method

---
+++ test/GenerateGameMain.groovy
@@ -0,0 +1,13 @@
+/**
+ * @author Lawrence Thatcher
+ */
+import TestGameDescription
+
+class GenerateGameMain
+{
+	public static void main(String[] args)
+	{
+		TestGameDescription t = new TestGameDescription()
+		t.testConvertToGDL()
+	}
+}
