1e4ee0537e8803c4040901e551e1b2bddce29f18
Lawrence Thatcher <lwthatcher@msn.com>
2016-03-02 12:48:06 -0700

    Players class now implements the Iterable interface and also has a next(Player) method

--- src/game/constructs/player/Players.groovy
+++ src/game/constructs/player/Players.groovy
@@ -15,7 +15,7 @@ import generator.Mutation
  * Stores information concerning the Players construct,
  * including number of players and the players' names.
  */
-class Players implements HasClauses, HasRolesClause, Gene
+class Players implements HasClauses, HasRolesClause, Gene, Iterable<Player>
 {
 	private List<Player> players = []
 
@@ -265,5 +265,20 @@ class Players implements HasClauses, HasRolesClause, Gene
 	int hashCode()
 	{
 		return players.hashCode()
+	}
+
+	@Override
+	Iterator<Player> iterator()
+	{
+		return this.players.iterator()
+	}
+
+	Player next(Player player)
+	{
+		int idx = players.indexOf(player)
+		idx++
+		if (idx == size())
+			return players[0]
+		return players[idx]
 	}
 }
